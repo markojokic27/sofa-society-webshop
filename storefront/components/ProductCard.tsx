95a2fd2a59d363e27d49af2809fb14382f984652
markojokic27 <jokicmarko98@gmail.com>
2025-03-17 09:19:33 +0100

    remove function call in ProductCard component

--- storefront/components/ProductCard.tsx
+++ storefront/components/ProductCard.tsx
@@ -5,54 +5,59 @@ import { HttpTypes } from "@medusajs/types";
 // Components
 import { LocalizedLink } from "./LocalizedLink";
 
-// Lib
-import { getProductPrice } from "@/lib/data/GetProductPrice";
+type Product = {
+  title: string;
+  collection: {
+    title: string;
+  };
+  cheapestPrice: {
+    calculated_price: string;
+    calculated_price_number: number;
+    original_price: string;
+    original_price_number: number;
+  };
+};
 
 export const ProductCard: React.FC<
   React.ComponentPropsWithoutRef<"div"> & {
     image: React.ReactNode;
     country: string;
     region: HttpTypes.StoreRegion;
-    product: HttpTypes.StoreProduct;
+    product: Product;
   }
-> = ({ image, country, region, product, className, ...rest }) => {
-  const { cheapestPrice } = getProductPrice({
-    product: product,
-  });
-  return (
-    <div {...rest} className={twMerge("w-full", className)}>
-      <LocalizedLink country={country} href={"/products/"}>
-        <div className="mb-4 w-full md:mb-6">{image}</div>
-        <div className="md:flex md:justify-between">
-          <div>
-            <p className="text-2xs text-gray-500 md:text-base md:text-black">
-              {product.title}
-            </p>
-            <p className="mt-1 hidden text-2xs text-gray-500 md:block">
-              {product.collection?.title}
-            </p>
-          </div>
-          <div className="md:text-right">
-            {cheapestPrice?.calculated_price && (
-              <p
-                className={twMerge(
-                  cheapestPrice.calculated_price_number !=
-                    cheapestPrice.original_price_number && "text-red-700",
-                  "text-2xs font-semibold md:text-base",
-                )}
-              >
-                {cheapestPrice?.calculated_price}
-              </p>
-            )}
-            {cheapestPrice?.calculated_price_number !=
-              cheapestPrice?.original_price_number && (
-              <p className="text-2xs text-gray-400 line-through md:text-base">
-                {cheapestPrice?.original_price}
-              </p>
-            )}
-          </div>
+> = ({ image, country, region, product, className, ...rest }) => (
+  <div {...rest} className={twMerge("w-full", className)}>
+    <LocalizedLink country={country} href={"/products/"}>
+      <div className="mb-4 w-full md:mb-6">{image}</div>
+      <div className="md:flex md:justify-between">
+        <div>
+          <p className="text-2xs text-gray-500 md:text-base md:text-black">
+            {product.title}
+          </p>
+          <p className="mt-1 hidden text-2xs text-gray-500 md:block">
+            {product.collection?.title}
+          </p>
         </div>
-      </LocalizedLink>
-    </div>
-  );
-};
+        <div className="md:text-right">
+          {product.cheapestPrice?.calculated_price && (
+            <p
+              className={twMerge(
+                product.cheapestPrice.calculated_price_number !=
+                  product.cheapestPrice.original_price_number && "text-red-700",
+                "text-2xs font-semibold md:text-base",
+              )}
+            >
+              {product.cheapestPrice?.calculated_price}
+            </p>
+          )}
+          {product.cheapestPrice?.calculated_price_number !=
+            product.cheapestPrice?.original_price_number && (
+            <p className="text-2xs text-gray-400 line-through md:text-base">
+              {product.cheapestPrice?.original_price}
+            </p>
+          )}
+        </div>
+      </div>
+    </LocalizedLink>
+  </div>
+);
